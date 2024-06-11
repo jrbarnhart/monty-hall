0eb9edd9669ee7b67667a62f2c55d51254733e28
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-10 23:46:25 -0400

    Set log on door click

--- src/Door.tsx
+++ src/Door.tsx
@@ -4,11 +4,20 @@ const Door = ({
   props: {
     number: number;
     setChoice: React.Dispatch<React.SetStateAction<number>>;
+    setLog: React.Dispatch<React.SetStateAction<string>>;
   };
 }) => {
-  const { number, setChoice } = props;
+  const { number, setChoice, setLog } = props;
+
+  const handleMouseDown = () => {
+    setChoice(number);
+    setLog(
+      `Door #${number} has a 33% to have the prize.\nThere is a ~66.6% chance the prize is behind the other doors.`
+    );
+  };
+
   return (
-    <div onMouseDown={() => setChoice(number)} className="door" role="button">
+    <div onMouseDown={handleMouseDown} className="door" role="button">
       <p>{`Door #${number}`}</p>
     </div>
   );
