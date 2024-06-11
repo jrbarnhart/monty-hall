d47c48d69d1e901a66c2de527ea97ae137d19728
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-10 22:50:57 -0400

    Pass setState to Doors

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   const [log, setLog] = useState<string>(
     "One door has a prize.\nTwo doors have a monster.\nChoose a door."
   );
+  const [choice, setChoice] = useState<number>(0);
 
   return (
     <main className="main-content">
@@ -18,13 +19,13 @@ function App() {
 
       <p className="log">{log}</p>
 
-      <div className="choice-chance">
+      <div className={`choice-chance choice-${choice.toString()}`}>
         <p>1/3 or ~33.3%</p>
       </div>
 
-      <Door props={{ label: "A" }} />
-      <Door props={{ label: "B" }} />
-      <Door props={{ label: "C" }} />
+      <Door props={{ number: 1, setChoice }} />
+      <Door props={{ number: 2, setChoice }} />
+      <Door props={{ number: 3, setChoice }} />
 
       <div className="other-chance">
         <p>2/3 or ~66.6%</p>
